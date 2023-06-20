1a39f669f53eb3ba0454f48cefe1d034841d3410
versatus <86527888+seacomit@users.noreply.github.com>
2023-06-20 17:19:43 -0400

    Add the clock class

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Clock : MonoBehaviour {
+
+    [SerializeField]
+    Transform hoursPivot, minutesPivot, secondsPivot;
+
+    void Awake () {
+        hoursPivot.localRotation = Quaternion.Euler(0, 0, -30);
+    }
+}
