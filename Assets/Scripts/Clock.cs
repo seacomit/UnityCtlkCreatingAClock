0f23803c28b2f4fd50cf5c8c405e9fb95970a3c7
versatus <86527888+seacomit@users.noreply.github.com>
2023-06-20 17:37:09 -0400

    Update the clock arms based on the current time.

--- Assets/Scripts/Clock.cs
+++ Assets/Scripts/Clock.cs
@@ -1,11 +1,17 @@
+using System;
 using UnityEngine;
 
 public class Clock : MonoBehaviour {
 
+    const float hoursToDegrees = -30f, minutesToDegrees = -6f, secondsToDegrees = -6f;
+
     [SerializeField]
     Transform hoursPivot, minutesPivot, secondsPivot;
 
-    void Awake () {
-        hoursPivot.localRotation = Quaternion.Euler(0, 0, -30);
+    void Update () {
+        var time = DateTime.Now;
+        hoursPivot.localRotation = Quaternion.Euler(0f, 0f, hoursToDegrees * time.Hour);
+        minutesPivot.localRotation = Quaternion.Euler(0f, 0f, minutesToDegrees * time.Minute);
+        secondsPivot.localRotation = Quaternion.Euler(0f, 0f, secondsToDegrees * time.Second);
     }
 }
